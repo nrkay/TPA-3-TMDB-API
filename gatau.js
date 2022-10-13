3f44f3231ff70576573a3e90805a6e6a63524735
nrkay <nkhairiyah018@gmail.com>
2022-10-13 21:03:30 +0700

    add null var in searchMovie

--- gatau.js
+++ gatau.js
@@ -10,7 +10,7 @@ let listMovie = async function movie() {
     let response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=2742b2a761b82c4cb62be61847d5acb7&sort_by=popularity.desc&page=1`)
     let result = await response.json()
     let movies = result.results
-    let variabelKosong = ''
+
     movies.forEach(item => {
        
         // let subCard = document.getElementById('subCard');
@@ -20,13 +20,13 @@ let listMovie = async function movie() {
         // contoh.innerHTML += `<h2>${item.original_title}</h2>`
         card.innerHTML += `
         <!-- Create Movie Card -->
-        <div class="col-3 mt-2">
+        <div class="col-lg-3 col-sm-6 mt-2">
           <div class="card" id="subCard">
             <div class="">
               <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" class="card-img-top" alt="" id="img-card">
               <div class="d-flex mb-3" id="descMovie">
                 <div class="p-2 m-1"  id="cardTitle">
-                  <p>${item.original_title}</p>
+                  <p><b>${item.original_title}</b></p>
                 </div>
                 <div class="ms-auto p-1 m-1"  id="cardRating">
                   <p>${item.vote_average}</p>
@@ -50,29 +50,29 @@ listMovie()
 
 // Search Movie
 
-let search = document.getElementById('search')
+let search = document.getElementById('form')
 const inputMovie = document.getElementById('inputMovie').value;
 
-search.addEventListener("click", (event)=>{
-
+search.addEventListener("submit", (event)=>{
+  
   event.preventDefault();
     let searchMovie = async (inputUser) => {
       let response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=2742b2a761b82c4cb62be61847d5acb7&query=${inputUser}&page=1`)
       let result = await response.json()
       let movies = result.results;
+      let print  = ''
       console.log(movies)
-      let kosong = ''
   
       movies.forEach(item => {
-        card.innerHTML += `
+        print += `
         <!-- Create Movie Card -->
-        <div class="col-3 mt-2">
+        <div class="col-lg-3 col-sm-6 mt-2">
           <div class="card" id="subCard">
             <div class="">
               <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" class="card-img-top" alt="" id="img-card">
               <div class="d-flex mb-3" id="descMovie">
                 <div class="p-2 m-1"  id="cardTitle">
-                  <p>${item.original_title}</p>
+                  <p><b>${item.original_title}</b></p>
                 </div>
                 <div class="ms-auto p-1 m-1"  id="cardRating">
                   <p>${item.vote_average}</p>
@@ -81,11 +81,16 @@ search.addEventListener("click", (event)=>{
             </div>
           </div>
         </div>`
+
+        card.innerHTML = print;
+        
       });
   
   
     }
     searchMovie(inputMovie);
+    return;
+
 })
 
 
